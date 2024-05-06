ba33800fb955917c24ba31ade74a48c78ebaebda
Ali Raza <soundsofnation29@gmail.com>
2024-05-06 23:32:17 +0500

    Added Committee Member doctype

---
+++ committee_management_system/committee_management_system/doctype/committee_member/committee_member.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2024, Ali Raza and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class CommitteeMember(Document):
+    def before_save(self):
+        self.full_name = f'{self.first_name} {self.last_name or ""}'
