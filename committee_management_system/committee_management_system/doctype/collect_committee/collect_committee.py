02ccc37963f70d0862bace5b53be291298b47b3f
Ali Raza <soundsofnation29@gmail.com>
2024-05-07 23:21:18 +0500

    Updated Committee Ledger to Submittable and updated ledger record on submitting collect committee

--- committee_management_system/committee_management_system/doctype/collect_committee/collect_committee.py
+++ committee_management_system/committee_management_system/doctype/collect_committee/collect_committee.py
@@ -3,11 +3,25 @@
 
 import frappe
 from frappe.model.document import Document
-from frappe.utils import cint
+from frappe.utils import cint, now
 from frappe import _
 
 
 class CollectCommittee(Document):
+    
     def before_submit(self):
         if cint(self.amount) <= 0:
             frappe.throw(_('Amount should be greater than zero'))
+            
+    def on_submit(self):
+        gl = frappe.new_doc('Committee Ledger')
+        gl.docstatus = 1
+        gl.posting_date = now()
+        gl.transaction_date = now()
+        gl.committee_member = self.member
+        gl.committee = self.committee
+        gl.debit = self.amount
+        gl.credit = 0
+        gl.remarks = f'Cash {self.amount} Received from {self.member} on {now()} against Committee {self.committee}'
+        gl.is_cancelled = 0
+        gl.insert()
